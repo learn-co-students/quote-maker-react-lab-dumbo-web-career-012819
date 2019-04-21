22424caf250a20e4e28a4bf33f7fbe2902ba4194
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-20 20:53:43 -0400

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -5,6 +5,10 @@ import QuoteCard from '../components/QuoteCard';
 class Quotes extends Component {
 
   render() {
+    const quotes = this.props.quotes.map(quote => {
+      return <QuoteCard key={quote.quoteId} quote={quote} />
+    })
+
     return (
       <div>
         <hr />
@@ -15,11 +19,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {quotes}
             </div>
           </div>
         </div>
@@ -28,5 +28,9 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {quotes: state.quotes}
+}
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps)(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,35 @@
+import uuid from 'uuid'
+
 export default (state = [], action) => {
-  return state;
+  let idx;
+  switch (action.type){
+
+  case "ADD_QUOTE":
+  	return [...state, action.quote]
+
+  case "REMOVE_QUOTE":
+  	idx = state.indexOf(action.quote);
+    return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+  case "UPVOTE_QUOTE":
+  	idx = state.indexOf(action.quote);
+  	let quote = state[idx]
+
+  	return [...state.slice(0, idx),
+  			{...quote, votes: quote.votes += 1},
+  			...state.slice(idx + 1)]
+
+  case "DOWNVOTE_QUOTE":
+  	idx = state.indexOf(action.quote);
+  	quote = state[idx]
+
+  	if(quote.votes > 0) {
+	  	return [...state.slice(0, idx),
+	  			{...quote, votes: quote.votes -= 1},
+	  			...state.slice(idx + 1)]
+  	}
+  
+  default:
+    return state;
+  }
 }
